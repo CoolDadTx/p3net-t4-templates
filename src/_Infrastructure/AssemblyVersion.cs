ebc4837f6ddd18bf2569001c919417f428fc9196
Michael Taylor <CoolDadTx@outlook.com>
2018-12-22 12:29:43 -0600

    Updated to SDK format. Moved config transform into a NuGet package. Updated to support auto-generated binding redirects.

--- src/_Infrastructure/AssemblyVersion.cs
+++ src/_Infrastructure/AssemblyVersion.cs
@@ -11,5 +11,5 @@
 [assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("15.0.0.0")]
-[assembly: AssemblyFileVersion("15.0.0.0")]
+[assembly: AssemblyVersion("15.1.0.0")]
+[assembly: AssemblyFileVersion("15.1.0.0")]
